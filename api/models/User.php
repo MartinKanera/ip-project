c45a7472a3a68c855d67823b791ac9686e0657e9
Martin Kaněra <marti.kanera@gmail.com>
2020-03-17 16:39:20 +0100

    feat(api): JWT auth added

--- api/models/User.php
+++ api/models/User.php
@@ -1,8 +1,12 @@
 <?php
+  use \Firebase\JWT\JWT;
+  use \Firebase\JWT\SignatureInvalidException;
+
   class User {
     private $conn;
     private $table = 'lide';
     private $sec_table = 'mistnosti';
+    private $key = '//Secret//super//Secret//';
 
     public $clovek_id;
     public $jmeno;
@@ -10,8 +14,6 @@
     public $pozice;
     public $plat;
     public $mistnost;
-    public $login;
-    public $password;
     public $admin;
     // Foreign key
     public $mistnost_id;
@@ -20,7 +22,7 @@
       $this->conn = $db;
     }
 
-    public function read ($order_by = 'jmeno', $sort = 'asc') {
+    public function read ($order_by, $sort) {
       $order_by = in_array(strtolower($order_by), array('jmeno', 'prijmeni', 'mistnost', 'telefon', 'pozice')) ? $order_by : 'jmeno';
       $sort = in_array(strtolower($sort), array('asc', 'desc')) ? $sort : 'ASC';
 
@@ -34,6 +36,47 @@
       $stmt->execute();
 
       return $stmt;
+    }
+
+    public function login($username, $password) {
+      require '../../vendor/autoload.php';
+
+      $query = 'SELECT clovek_id, login, hash, admin, jmeno, prijmeni FROM ' . $this->table . ' WHERE login = :login';
+
+      $stmt = $this->conn->prepare($query);
+
+      $stmt->bindParam(':login', $username);
+
+      $stmt->execute();
+      
+      if($stmt->rowCount() > 0) {
+        $row = $stmt->fetch();
+        extract($row);
+
+        if(password_verify($password, $hash)) {
+          session_start();
+          
+          $payload = array(
+            'user_id' => $clovek_id,
+            'first_name' => $jmeno,
+            'last_name' => $prijmeni,
+            'admin' => $admin
+          );
+
+          $jwt =JWT::encode($payload, $this->key);
+
+          return json_encode(array(
+            'message' => 'Login successful',
+            'jwt' => $jwt
+          ));
+        }
+        return json_encode(array(
+          'message' => 'Wrong password'
+        ));
+      }
+      return json_encode(array(
+        'message' => 'Wrong username'
+      ));
     }
   } 
 ?>
