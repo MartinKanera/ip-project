9374837ef23675ef79a43ede8bf25d993cb9dc3f
Martin Kaněra <marti.kanera@gmail.com>
2020-03-19 13:03:58 +0100

    feat(api): DELETE finished

---
+++ api/api/rooms_keys.php
@@ -0,0 +1,35 @@
+<?php
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once('../config/Database.php');
+  include_once('../models/Room.php');
+
+  $database = new Database();
+  $db = $database->connect();
+
+  $room = new Room($db);
+
+  $result = $room->rooms_keys();
+
+  if($result->rowCount() > 0) {
+    $rooms = array();
+    while($row = $result->fetch()) {
+      extract($row);
+
+      $single_room = array(
+        'id' => $mistnost_id,
+        'name' => $nazev
+      );
+
+      array_push($rooms, $single_room);
+    }
+
+    echo json_encode($rooms, 256);
+  } else {
+    http_response_code(404);
+    echo json_encode(array(
+      'message' => 'No rooms found'
+    ));
+  }
+?>
---
+++ api/api/user/delete.php
@@ -0,0 +1,58 @@
+<?php
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-methods, Authorization');
+
+  require '../../vendor/autoload.php';
+  use \Firebase\JWT\JWT;
+  use \Firebase\JWT\SignatureInvalidException;
+
+  $header = apache_request_headers();
+  $key = '//Secret//super//Secret//';
+  $jwt = explode(' ', $header['Authorization'])[1];
+
+  try {
+    $payload = JWT::decode($jwt, $key, array('HS256'));
+  } catch (Exception $e) {
+    http_response_code(401);
+    echo json_encode(array(
+      'message' => 'Invalid token'
+    ));
+    die();
+  }
+
+  $id = filter_input(
+    INPUT_POST,
+    'id',
+    FILTER_VALIDATE_INT,
+    ["options" => ["min_range" => 0]]
+  );
+  
+  if(!$id) {
+    http_response_code(400);
+    echo json_encode(array(
+      'message' => 'Parameters are not fulfilled'
+    ));
+    exit;
+  }
+
+  if($payload->admin > 0) {
+    include_once('../../config/Database.php');
+    include_once('../../models/User.php');
+
+    $database = new Database();
+    $db = $database->connect();
+
+    $user = new User($db);
+
+    if($user->delete($id)) {
+      http_response_code(204);
+    } else {
+      http_response_code(409);
+      echo json_encode(array(
+        'message' => 'Failed to delete user'
+      ));
+    }
+  }
+?>
