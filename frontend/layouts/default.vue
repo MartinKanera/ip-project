07df1be01fc25159a5e1849287499c3ea855067d
Martin Kaněra <marti.kanera@gmail.com>
2020-03-24 21:04:50 +0100

    fix(nuxt-init): part 2

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app dark>
+    <div>
+      <v-toolbar color="secondary">
+        <v-toolbar-title>Database browser</v-toolbar-title>
+      </v-toolbar>
+    </div>
+    <nuxt />
+  </v-app>
+</template>
+
+<style lang="sass"></style>
+
+<script></script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="login-wrap">
+    <v-card class="login" color="secondary" flat>
+      <form>
+        <v-text-field
+          v-model="username"
+          class="mb-6"
+          :counter="12"
+          label="Login"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          class="mb-6"
+          label="Password"
+          type="password"
+          required
+        ></v-text-field>
+        <v-btn class="custom-btn" color="accent" @click="login">Login</v-btn>
+      </form>
+    </v-card>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.login-wrap
+  width: 33%
+  margin: auto
+
+  .login
+    padding: 50px
+    width: 100%
+</style>
+
+<style scoped>
+@media only screen and (max-width: 978px) {
+  .login-wrap {
+    width: 75%;
+  }
+}
+</style>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from '@vue/composition-api';
+import axios from 'axios';
+
+export default defineComponent({
+  setup() {
+    const username = ref('');
+    const password = ref('');
+    const idk = ref(null);
+
+    async function login() {
+      try {
+        const response = await axios.post(
+          'http://a2017kanema.delta-studenti.cz/api/auth/login.php',
+          {
+            data: {
+              login: 'franta',
+              password: 'Heslo123'
+            }
+          }
+        );
+
+        console.log(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    return {
+      login,
+      username,
+      password
+    };
+  }
+});
+</script>
