d052ce06ed3dbdbae34d2c4eb97e5474759f1612
Martin Kaněra <marti.kanera@gmail.com>
2020-03-24 20:56:30 +0100

    fix(nuxt-init): part 1

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,91 @@
+import { Configuration } from '@nuxt/types';
+
+const config: Configuration = {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  compilerOptions: {
+    types: ['vuetify']
+  },
+  /*
+   ** Global CSS
+   */
+  css: ['~assets/styles.css'],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['@/plugins/core.ts'],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    '@nuxt/typescript-build',
+    // Doc: https://github.com/nuxt-community/stylelint-module
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/vuetify'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    '@nuxtjs/proxy'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: '#121212',
+          secondary: '#222831',
+          accent: '#29a19c'
+        }
+      }
+    }
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    // extend(config, ctx) {}
+  }
+};
+
+export default config;
